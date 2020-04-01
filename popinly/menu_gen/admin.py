b45171d30043187b5e29914e76c9d75857b96181
Geoff Beamish <geoff@Geoffs-MacBook-Air.local>
2020-03-31 22:43:57 -0400

    extend admin to use sections

--- popinly/menu_gen/admin.py
+++ popinly/menu_gen/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
-from .models import Menu
-from .models import MenuItem
+from .models import Menu, MenuSection, MenuItem
 
 admin.site.register(Menu)
+admin.site.register(MenuSection)
 admin.site.register(MenuItem)
