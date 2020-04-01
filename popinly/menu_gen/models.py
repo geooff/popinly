3070349f5446b0cbe409befc982bafdc8749970b
Geoff Beamish <geoff@Geoffs-MacBook-Air.local>
2020-03-31 22:44:47 -0400

    update model naming and add menu sections

--- popinly/menu_gen/models.py
+++ popinly/menu_gen/models.py
@@ -1,26 +1,37 @@
-import uuid
+from uuid import uuid4
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth import get_user_model
 
 
 class Menu(models.Model):
-    menu_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    uuid = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     restaurant_name = models.CharField(max_length=200)
-    menu_title = models.CharField(max_length=200)
+    title = models.CharField(max_length=200)
     created_at = models.DateField(default=timezone.now)
 
     def __str__(self):
         return self.menu_title
 
 
+class MenuSection(models.Model):
+    uuid = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    menu = models.ForeignKey(Menu, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    description = models.CharField(max_length=200)
+    created_at = models.DateField(default=timezone.now)
+
+    def __str__(self):
+        return self.item_name
+
+
 class MenuItem(models.Model):
-    item_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    parent_menu = models.ForeignKey(Menu, on_delete=models.CASCADE)
-    item_name = models.CharField(max_length=200)
-    item_description = models.CharField(max_length=200)
-    item_price = models.DecimalField(max_digits=5, decimal_places=2)
+    uuid = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    section = models.ForeignKey(MenuSection, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    description = models.CharField(max_length=200)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
     created_at = models.DateField(default=timezone.now)
 
     def __str__(self):
