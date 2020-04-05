05ef057399da5b1ad1d0c899b53096ceee5fdd8f
Geoff Beamish <geoff@Geoffs-MacBook-Air.local>
2020-04-04 23:48:33 -0400

    Init menu_gen namespace

--- popinly/menu_gen/urls.py
+++ popinly/menu_gen/urls.py
@@ -2,10 +2,11 @@ from django.urls import path
 
 from . import views
 
+app_name = "menu_gen"
 urlpatterns = [
-    path("", views.MenuListView.as_view(), name="menu_index"),
-    path("add/", views.MenuCreateView.as_view(), name="menu_add"),
-    path("edit/<uuid:pk>", views.MenuItemsUpdateView.as_view(), name="menu_edit"),
-    path("delete/<uuid:pk>", views.MenuDelete.as_view(), name="menu_delete"),
-    path("<uuid:pk>", views.MenuDetailView.as_view(), name="menu_detail"),
+    path("", views.MenuListView.as_view(), name="index"),
+    path("add/", views.MenuCreateView.as_view(), name="add"),
+    path("edit/<uuid:pk>", views.MenuItemsUpdateView.as_view(), name="edit"),
+    path("delete/<uuid:pk>", views.MenuDelete.as_view(), name="delete"),
+    path("<uuid:pk>", views.MenuDetailView.as_view(), name="detail"),
 ]
