d357d9dd2de1ce2f1abe63beb2db12e74d3ffc46
Geoff Beamish <geoff@Geoffs-MacBook-Air.local>
2020-04-04 23:49:35 -0400

    add register logic

--- popinly/popinly/views.py
+++ popinly/popinly/views.py
@@ -1,5 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, reverse
+from django.views.generic.edit import FormView
+from django.contrib.auth.forms import UserCreationForm
 
 
 def index(request):
     return render(request, "index.html")
+
+
+class RegisterView(FormView):
+    template_name = "registration/register.html"
+    form_class = UserCreationForm
+
+    def form_valid(self, form):
+        form.save()
+        return super(RegisterView, self).form_valid(form)
+
+    def get_success_url(self):
+        return reverse("index")
