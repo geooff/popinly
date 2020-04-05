fbcc17e4d1923d24ccc017bc0e83a9948b2a0dfe
Geoff Beamish <geoff@Geoffs-MacBook-Air.local>
2020-04-04 23:49:59 -0400

    add signup routs

--- popinly/popinly/urls.py
+++ popinly/popinly/urls.py
@@ -2,13 +2,13 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf.urls import url
 
+
 from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
     path("generator/", include("menu_gen.urls")),
     path("admin/", admin.site.urls),
-    path(
-        "accounts/", include("django.contrib.auth.urls")
-    ),  # Add Django site authentication urls (for login, logout, password management)
+    path("accounts/", include("django.contrib.auth.urls")),
+    path("signup/", views.RegisterView.as_view(), name="register"),
 ]
